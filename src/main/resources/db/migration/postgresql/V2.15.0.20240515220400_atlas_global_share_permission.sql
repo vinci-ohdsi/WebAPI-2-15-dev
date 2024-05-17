4cf2647aca7f48cb52b6c71486699b231dfbb4fb
pieterlukasse <pieterlukasse@gmail.com>
2024-05-17 14:15:37 +0200

    Update src/main/resources/db/migration/postgresql/V2.15.0.20240515220400_atlas_global_share_permission.sql

--- src/main/resources/db/migration/postgresql/V2.15.0.20240515220400_atlas_global_share_permission.sql
+++ src/main/resources/db/migration/postgresql/V2.15.0.20240515220400_atlas_global_share_permission.sql
@@ -1,5 +1,5 @@
 INSERT INTO ${ohdsiSchema}.sec_permission
 (value, description)
 values
-('cohortdefinition:global:share:put', 'special permission (intended for admins) that allows the user to share any cohort with a "global reader role"')
+('artifact:global:share:put', 'special permission (intended for admins) that allows the user to share any artifact with a "global reader role"')
 ;
